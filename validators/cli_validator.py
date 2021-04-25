9e86ef8f50d87c4ab1a027c12f8e6994a7486dea
Avital Fine <79420960+AvitalFineRedis@users.noreply.github.com>
2021-04-25 14:23:51 +0300

    Update cli_validator.py

--- validators/cli_validator.py
+++ validators/cli_validator.py
@@ -3,9 +3,10 @@ import tempfile
 import os
 
 class CliValidator(BaseValidator):
+    """A subclass of BaseValidator. Validates CLI contents."""
     
     def is_valid(self, content:str) -> bool:
-        
+        """Returns True is the content is valid CLI content. Otherwise, False"""
         fd, abspath = tempfile.mkstemp(suffix=".yaml")
         with open(abspath, 'w+') as f:
             f.write(content)
